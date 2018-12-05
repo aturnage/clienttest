cfa53649a402cb512310c3abf3cb74f496878981
TurnageA <TurnageA@stlouis-mo.gov>
2018-12-05 13:15:30 -0600

    Built basic HTML, javascript array, for test

---
+++ scripts/clientinfo.js
@@ -0,0 +1,12 @@
+var clients =
+{
+    name:"Adbul Kaba Abdullah",
+    description:"Dynamic branding brochure collateral for political campaign",
+    images:"Abdul pic here",
+    well:"Guess"
+} ,
+{
+    name:"National Association of Spellman College",
+    description:"Advertising collateral for fundriaisng event",
+    images:url('BusinessCard-StarShadow.jpg')
+    }
